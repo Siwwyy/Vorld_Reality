b12796d55bdc3582ea46d0e99c2d1c769d72b97b
krecichwostk <kamil.krecichwost@ttpsc.pl>
2019-11-30 15:57:28 +0100

    Clean up. Erased outdated projects.

---
+++ Vorld_Reality/Assets/Scripts/Camera_scripts/Move.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float input_X, input_Z;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input_X = Input.GetAxis("Horizontal");
+        input_Z = Input.GetAxis("Vertical");
+
+        if (input_X != 0)
+        {
+            Rotate_Camere();
+        }
+        if (input_Z != 0)
+        {
+            Move_Camera();
+        }
+    }
+
+    private void Move_Camera()
+    {
+        transform.position += transform.forward * input_Z * Time.deltaTime * 100;
+    }
+
+    private void Rotate_Camere()
+    {
+        transform.Rotate(new Vector3(0f, input_X * Time.deltaTime * 100, 0f));
+    }
+}
